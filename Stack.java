6bf533d8f2198e73038f9ca86f28e86e115c2ee5
James Sanii <james@ip-172-31-8-86.ca-central-1.compute.internal>
2019-03-08 19:06:14 +0000

    Stack program

---
+++ Stack.java
@@ -0,0 +1,34 @@
+/****************************************************************************
+ *
+ * Created by: James Lee
+ * Created on: Mar 2019
+ * Created for: ICS4U
+ * This program
+ *
+ *
+ ****************************************************************************/
+
+import java.util.ArrayList;
+
+class Stack {
+
+    private final ArrayList<Integer> myStack = new ArrayList<Integer>();
+
+    public void push(int number)
+    {
+        myStack.add(number);
+    }
+
+    public String toString()
+    {
+        String output = "";
+
+        for (Integer count : myStack)
+        {
+            output += count + "\n";
+        }
+
+        return output.toString();
+    }
+
+}
