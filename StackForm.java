6a18bddf4967df80b6d204547c499284d7a0f6d9
James Sanii <james@ip-172-31-8-86.ca-central-1.compute.internal>
2019-03-10 02:37:06 +0000

    Updated comments

--- StackForm.java
+++ StackForm.java
@@ -1,8 +1,20 @@
+/****************************************************************************
+ *
+ * Created by: James Lee
+ * Created on: Mar 2019
+ * Created for: ICS4U
+ * This program uses the class Stack, and pushes three numbers to it
+ *
+ ****************************************************************************/
+
 class StackForm
 {
     public static void main(String[] args)
     {
+        // Instantiates stack
         Stack stack = new Stack();
+
+        // Push 3 numbers
         stack.push(29);
         stack.push(2);
         stack.push(10);
